99e2d3e586c77c946a694c34847575e075b5ef8c
brave <arthur.braveheart@estudante.ufjf.br>
2025-04-08 17:51:50 -0300

    updates

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,3 +153,4 @@ CHANNEL_LAYERS = {
         }
     }
 }
+sys.path.append(os.path.join(BASE_DIR, 'core/apps'))
